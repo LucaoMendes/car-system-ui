dcef9cc0f984ddc534f727f49897ac0d6eeaf881
LucaoMendes <lucas1999mendes@gmail.com>
2023-11-27 19:57:33 -0300

    [App] Ajustes no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,11 @@ import { FirstPhaseComponent } from './signup/first-phase/first-phase.component'
 import { SecondPhaseComponent } from './signup/second-phase/second-phase.component'
 import { SignupPhaseComponent } from './signup/signup-phase/signup-phase.component'
 import { ThirdPhaseComponent } from './signup/third-phase/third-phase.component'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { LoadingComponent } from './shared/components/loading/loading.component'
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarsTableComponent } from './dashboard/components/cars-table/cars-table.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { DeleteModalComponent } from './dashboard/components/delete-modal/delete-modal.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +27,14 @@ import { DashboardComponent } from './dashboard/dashboard.component'
     ThirdPhaseComponent,
     LoadingComponent,
     DashboardComponent,
+    CarsTableComponent,
+    NavbarComponent,
+    DeleteModalComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    FontAwesomeModule
+    AppRoutingModule
   ],
   bootstrap: [AppComponent]
 })
